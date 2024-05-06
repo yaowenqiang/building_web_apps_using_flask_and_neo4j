c56b4afe1b747d47bb5b54701ebd4c0c3196462d
yaowenqiang <yaowenqiang111@163.com>
2024-05-06 23:32:04 +0800

    add get, post request and url parameter demo

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # return "Hello World"
     # data = [1,2,3]
     data = [{'x':1}, {'x':2}, {'x':5}]
     # return render_template('index.html', message="Hello World message")
-    return render_template('index.html', data=data)
+    message = 'this is a get request'
+    if request.method == 'POST':
+        message = 'this is a post request'
+    return render_template('index.html', data=data, message=message)
 
-@app.route('/about')
-def about():
-    return "about"
+@app.route('/about/<something>')
+def about(something):
+    return f'This page is about {something}'
 
 @app.route('/err')
 def err():
